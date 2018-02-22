ba17a3c96a458d3e068ad9cbf4fbb8617285f9dd
DavidMarck <david.marck88@gmail.com>
2018-02-22 18:48:56 +0000

    comments

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include "../include/main.h"
 
+/**
+ * Prints the command lines starter
+ */
 void prompt()
 {
     printf("my_sh > ");
 }
 
+/**
+ * Reads a string as a command
+ * @param command string to be read as a command
+ */
 void readCommand(char* command)
 {
     int commandLength = strlen(command);
@@ -20,6 +27,11 @@ void readCommand(char* command)
     }
 }
 
+/**
+ * Progam
+ * @param argc arguments count
+ * @param argv array of arguments
+ */
 int main(int argc, char** argv)
 {
     char commandLine[255];
--- src/util.c
+++ src/util.c
@@ -1,5 +1,9 @@
 #include "../include/util.h"
 
+/**
+ * Returns a string in lower case
+ * @param string string to be returned in lower case
+ */
 char* stringToLower(char* string)
 {
     for(int i = 0; i = strlen(string); i++)
